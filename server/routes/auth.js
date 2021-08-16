6b0856995a56d813326cfb30e2903613e27e786c
Rupam Shil <rupamshil111@gmail.com>
2021-08-16 13:04:40 +0530

    backend signup,signin testing done

---
+++ server/routes/auth.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+router.post(
+	'/signup',
+	[
+		check('email', 'Please Provide a valid email').isEmail(),
+		check(
+			'password',
+			'Please provide a password of more than 6 character'
+		).isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const { name, email, password } = req.body;
+
+		const errors = validationResult(req);
+		if (errors.isEmpty()) {
+			const checkUser = await User.findOne({ email: email });
+			if (checkUser) {
+				res.status(400).json({ error: 'User Already Exist' });
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 10);
+			let user = new User({
+				name: name,
+				email: email,
+				password: hashedPassword,
+			});
+			user = await user.save();
+
+			res.send('authed');
+		} else {
+			res.status(400).json({ errors: errors.array() });
+		}
+	}
+);
+
+router.post('/login', async (req, res) => {
+	let user = await User.findOne({ email: req.body.email });
+	if (!user) {
+		res.status(400).json({ error: 'User not registered' });
+	} else {
+		const isPasswordMatched = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!isPasswordMatched) {
+			res.status(400).json({ error: 'Invalid Credentials' });
+		} else {
+			res.json({ name: user.name, email: user.email });
+		}
+	}
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const cors = require('cors');
+
+mongoose.connect('mongodb://localhost/advacedtodo', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+mongoose.connection
+	.once('open', () => {
+		console.log('DB connected');
+	})
+	.on('error', () => {
+		console.log('An error occurred');
+	});
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/auth', authRoute);
+app.listen(9000, () => {
+	console.log('Running on port 9000');
+});
